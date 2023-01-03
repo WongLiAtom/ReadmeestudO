337ecac8d52b91e23114b44e95844a315e02ebe8
WongLiAtom <88535007+WongLiAtom@users.noreply.github.com>
2023-01-03 08:34:51 +0000

    states

--- src/main/java/ch/pschatzmann/stocks/accounting/Transaction.java
+++ src/main/java/ch/pschatzmann/stocks/accounting/Transaction.java
@@ -163,4 +163,187 @@ public class Transaction implements Serializable, Comparable<Transaction> {
 	public BuyOrSell getBuyOrSell() {
 		if (this.getQuantity() == 0) {
 			return BuyOrSell.NA;
-		} else if (this.getQuantity() > 
+		} else if (this.getQuantity() > 0) {
+			return BuyOrSell.Buy;
+		} else {
+			return BuyOrSell.Sell;
+		}
+	}
+
+	/**
+	 * fills the order with the indicated information. The fees are calculated with
+	 * the help of the fees model. The quantity is reduced if there is not enogh
+	 * cash
+	 * 
+	 * @param date
+	 * @param quantity
+	 * @param fillPrice
+	 * @param account
+	 */
+	public synchronized void fill(Date date, long quantity, double fillPrice, IAccount account) {
+		if (account.getAccount().isMargin()) {
+			this.quantity = quantity;
+		} else {
+			double currentCash = account.getCash(date);
+			if (quantity > 0) {
+				// LOG.info("fill qty:"+quantity+" price:"+fillPrice+" currentCash:"+
+				// currentCash+" date "+Context.format(date));
+				if (quantity * fillPrice <= currentCash) {
+					this.quantity = quantity;
+				} else {
+					Long oldQty = quantity;
+					this.quantity = (long) (currentCash / fillPrice);
+					// the quantity can not be negative
+					if (this.quantity < 0) {
+						this.quantity = 0;
+					}
+
+					LOG.info("Not enough cash to fill the full order: " + this + " setting quantity to " + this.quantity
+							+ " instead of " + oldQty);
+				}
+			}
+		}
+
+		this.filledPrice = fillPrice;
+		this.date = date;
+		this.fees = account.getAccount().getFeesModel().getFeesPerTrade(quantity, fillPrice * quantity);
+		this.setStatus(Status.Filled);
+		LOG.debug("-> fill " + this.getStockID() + " " + Context.format(this.getDate()) + " " + this.quantity + " * "
+				+ fillPrice);
+	}
+
+	/**
+	 * Fills the order with the indicated information. 
+	 * 
+	 * @param date
+	 * @param quantity
+	 * @param fillPrice
+	 * @param fees
+	 */
+	public synchronized void fill(Date date, long quantity, double fillPrice, double fees) {
+		this.date = date;
+		this.quantity = quantity;
+		this.filledPrice = fillPrice;
+		this.fees = fees;
+		this.setStatus(Status.Filled);
+	}
+
+	/**
+	 * if cashIn we return the cash value if we sell (= negative quantity) the cash
+	 * increases. if we buy (= positive quantity) the cash decreases
+	 * 
+	 * @return
+	 */
+	public double getImpactOnCash() {
+		return this.cash != 0 ? cash
+				: this.transactionType == Type.CashTransfer ? 0.0 : (-this.quantity * filledPrice) - getFees();
+	}
+
+	public void setImpactOnCash(double value) {
+		this.cash = value;
+	}
+
+	@JsonIgnore
+	public double getPurchasedValue() {
+		return this.quantity * filledPrice;
+	}
+
+	/**
+	 * Returns true if the order is to buy stock on the market (indicated by a
+	 * positive quantity)
+	 * 
+	 * @return
+	 */
+	@JsonIgnore
+	public boolean isBuy() {
+		return this.getQuantity() > 0;
+	}
+
+	/**
+	 * Returns true if the order is to sell stock on the market (indicated by a
+	 * negative quantity)
+	 * 
+	 * @return
+	 */
+	@JsonIgnore
+	public boolean isSell() {
+		return this.getQuantity() < 0;
+	}
+
+	/**
+	 * Determine if the order is filled (true) or still open (false)
+	 * 
+	 * @return
+	 */
+	@JsonIgnore
+	public boolean isFilled() {
+		return this.getStatus() == Status.Filled;
+	}
+
+	/**
+	 * Determine if the order is filled (false) or still open (true)
+	 * 
+	 * @return
+	 */
+	@JsonIgnore
+	public boolean isOpen() {
+		return !isFilled();
+	}
+
+	@JsonIgnore
+	public boolean isCashTransfer() {
+		return this.transactionType == Type.CashTransfer;
+	}
+
+	@Override
+	public int compareTo(Transaction o) {
+		int result = this.getDate().compareTo(o.getDate());
+		if (result == 0) {
+			result = StockID.compare(this.getStockID(), o.getStockID());
+			if (result == 0) {
+				result = this.getID().compareTo(o.getID());
+			}
+		}
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append(this.stockID);
+		sb.append(" ");
+		sb.append(Context.format(this.getDate()));
+		sb.append(" ");
+		if (this.isCashTransfer()) {
+			sb.append(this.getImpactOnCash());
+		} else {
+			sb.append(this.getQuantity());
+			sb.append(" * ");
+			sb.append(this.getFilledPrice());
+		}
+
+		if (!isFilled()) {
+			sb.append(" (not filled!)");
+		}
+		sb.append(" ");
+		sb.append(this.getComment());
+		return sb.toString();
+	}
+
+	public void cancel() {
+		LOG.info("cancel "+this);
+		this.setStatus(Status.Cancelled);
+		this.fees = 0;
+		this.filledPrice = 0.0;
+	}
+	
+	public void cancel(String comment) {
+		LOG.info("cancel "+this);
+		this.setStatus(Status.Cancelled);
+		this.setComment(comment);
+		this.fees = 0;
+		this.filledPrice = 0.0;
+	}
+	
+
+	public boolean isAc
