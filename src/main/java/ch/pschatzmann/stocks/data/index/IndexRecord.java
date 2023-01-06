f3a898eebf44580443305f8bfaaf21d98702cb2c
WongLiAtom <88535007+WongLiAtom@users.noreply.github.com>
2023-01-06 14:19:39 +0000

    msm: vidc: use %pK instead of %p which respects kptr_restrict sysctl

---
+++ src/main/java/ch/pschatzmann/stocks/data/index/IndexRecord.java
@@ -0,0 +1,17 @@
+package ch.pschatzmann.stocks.data.index;
+
+import java.io.Serializable;
+
+import ch.pschatzmann.stocks.StockID;
+
+/**
+ * Individual Record for Stock Index
+ * 
+ * @author pschatzmann
+ *
+ */
+public class IndexRecord implements  Serializable {
+	/**
+	 * 
+	 */
+	p
