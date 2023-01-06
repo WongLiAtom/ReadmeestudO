46d73e4312ce525bf2f840086451e74d6103ace5
WongLiAtom <88535007+WongLiAtom@users.noreply.github.com>
2023-01-06 15:33:28 +0000

    remove trailing whitespaces

--- src/main/java/ch/pschatzmann/stocks/data/index/IndexRecord.java
+++ src/main/java/ch/pschatzmann/stocks/data/index/IndexRecord.java
@@ -14,4 +14,43 @@ public class IndexRecord implements  Serializable {
 	/**
 	 * 
 	 */
-	p
+	private static final long serialVersionUID = 1L;
+	private StockID id;
+	private double weight;
+
+	public IndexRecord(StockID stockID, Double weight) {
+		this.id = stockID;
+		this.weight = weight;
+	}
+
+	public IndexRecord(StockID stockID, String weight) {
+		this.id = stockID;
+		this.weight = Double.parseDouble(weight);
+	}
+
+	public StockID getId() {
+		return id;
+	}
+
+	public void setId(StockID id) {
+		this.id = id;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append(id);
+		sb.append(":");
+		sb.append(""+weight);
+		return sb.toString();
+	}
+
+}
