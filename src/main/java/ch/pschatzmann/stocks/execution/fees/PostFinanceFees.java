ad20dea20ddc3d5a01d0e697f391c42abeaf3cb9
WongLiAtom <88535007+WongLiAtom@users.noreply.github.com>
2023-01-08 20:56:06 +0000

    Removed a completed task.

--- src/main/java/ch/pschatzmann/stocks/execution/fees/PostFinanceFees.java
+++ src/main/java/ch/pschatzmann/stocks/execution/fees/PostFinanceFees.java
@@ -19,3 +19,25 @@ public class PostFinanceFees implements IFeesModel {
 		if (value <= 10000) {
 			return 40;
 		}
+		if (value <= 15000) {
+			return 50;
+		}
+		if (value <= 20000) {
+			return 70;
+		}
+		if (value <= 30000) {
+			return 95;
+		}
+		if (value <= 50000) {
+			return 130;
+		}
+		if (value <= 100000) {
+			return 180;
+		}
+		if (value <= 150000) {
+			return 270;
+		}
+		return 350;
+	}
+
+}
