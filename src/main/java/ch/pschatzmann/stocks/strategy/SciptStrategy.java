1b516dc7a22c499cf6d4a0e437b9a329aea06411
WongLiAtom <88535007+WongLiAtom@users.noreply.github.com>
2023-02-13 17:38:46 +0000

    more names

--- src/main/java/ch/pschatzmann/stocks/strategy/SciptStrategy.java
+++ src/main/java/ch/pschatzmann/stocks/strategy/SciptStrategy.java
@@ -33,3 +33,67 @@ public class SciptStrategy extends CommonTradingStrategy implements ITradingStra
 	private ScriptEngine engine;
 	private String script;
 	private String engineName;
+
+	public SciptStrategy(IStockData stockData) {
+		super(stockData);
+	}
+
+	public SciptStrategy(IStockData stockData, String engineName, String script) {
+		super( stockData);
+		this.engineName = engineName;
+		this.script = script;
+	}
+
+	@Override
+	public List<InputParameterName> getParameterOptimizationSequence() {
+		return new ArrayList();
+	}
+
+	@Override
+	public Strategy buildStrategy(BarSeries timeSeries) {
+		try {
+			if (engine == null) {
+				ScriptEngineManager factory = new ScriptEngineManager();
+				engine = factory.getEngineByName(engineName);
+			}
+			if (engine==null) {
+				throw new RuntimeException("The engine could not be loaded: "+engineName);
+			}
+			
+			engine.getContext().setAttribute("timeSeries", timeSeries, ScriptContext.ENGINE_SCOPE);
+			engine.getContext().setAttribute("strategy", null, ScriptContext.ENGINE_SCOPE);
+			Object obj = engine.eval(script);
+			if (engine instanceof Invocable) {
+				Invocable inv = (Invocable) engine;
+				obj = inv.invokeFunction("execute", timeSeries);
+			}
+			Strategy result = null;
+			if (obj instanceof Strategy) {
+				result = (Strategy) obj;
+			} else {
+				result = (Strategy) engine.get("strategy");
+			}
+			return result;
+		} catch (Exception e) {
+			LOG.error(e.getLocalizedMessage(), e);
+			throw new RuntimeException(e);
+		}
+	}
+
+	public String getScript() {
+		return script;
+	}
+
+	public void setScript(String script) {
+		this.script = script;
+	}
+
+	public String getEngineName() {
+		return engineName;
+	}
+
+	public void setEngineName(String engineName) {
+		this.engineName = engineName;
+	}
+
+}
---
+++ src/main/java/ch/pschatzmann/stocks/strategy/TradingStrategyFactory.java
@@ -0,0 +1,136 @@
+package ch.pschatzmann.stocks.strategy;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.ta4j.core.Strategy;
+import org.ta4j.core.BarSeries;
+
+import ch.pschatzmann.stocks.Context;
+import ch.pschatzmann.stocks.IStockData;
+import ch.pschatzmann.stocks.IStockID;
+import ch.pschatzmann.stocks.StockData;
+import ch.pschatzmann.stocks.data.universe.IUniverse;
+import ch.pschatzmann.stocks.data.universe.RandomUniverse;
+import ch.pschatzmann.stocks.errors.CommonException;
+import ch.pschatzmann.stocks.errors.UniverseException;
+import ch.pschatzmann.stocks.input.IReader;
+import ch.pschatzmann.stocks.strategy.optimization.IOptimizableTradingStrategy;
+import ch.pschatzmann.stocks.utils.FileUtils;
+
+/**
+ * Factory which lists and creates the available trading strategies
+ * 
+ * @author pschatzmann
+ *
+ */
+public class TradingStrategyFactory {
+	private static final Logger LOG = LoggerFactory.getLogger(TradingStrategyFactory.class);
+
+	public enum StrategyEnum {
+		CCICorrectionStrategy, GlobalExtremaStrategy, MovingMomentumStrategy, RSI2Strategy
+	};
+
+	/**
+	 * Creates a single ITradingStrategy
+	 * @param strategy
+	 * @param stockData
+	 * @return
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 * @throws IllegalArgumentException
+	 * @throws InvocationTargetException
+	 */
+	public static ITradingStrategy create(Class<ITradingStrategy> strategy,  IStockData stockData)
+			throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		try {
+			Constructor[] ca = strategy.getDeclaredConstructors();
+			if (ca.length != 1) {
+				throw new IllegalArgumentException("We support only one construcutor!");
+			}
+			return (ITradingStrategy) ca[ca.length - 1].newInstance(stockData);
+		} catch (IllegalArgumentException ex) {
+			LOG.error("Illigal argument of construcotor for " + strategy);
+			throw ex;
+		}
+	}
+
+	/**
+	 * 	 * Creates a single ITradingStrategy
+	 *
+	 * @param strategy
+	 * @param stockData
+	 * @return
+	 */
+	public static ITradingStrategy create(String strategy,  IStockData stockData) {
+		try {
+			Class<ITradingStrategy> c = (Class<ITradingStrategy>) Class
+					.forName("ch.pschatzmann.stocks.strategy." + strategy);
+			return create(c, stockData);
+		} catch(Exception ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+	
+	/**
+	 * 
+	 * Creates a single ITradingStrategy
+	 * 
+	 * @param strategy
+	 * @param stockData
+	 * @return
+	 */
+	public static ITradingStrategy create(Strategy strategy, IStockData stockData) {
+		return new CommonTradingStrategy(stockData) {
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public Strategy buildStrategy(BarSeries timeSeries) {
+				return strategy;
+			}			
+		};		
+	}
+
+	/**
+	 * Returns a detailed description for the strategy name
+	 * @param strategy
+	 * @return
+	 */
+	public static String getStrategyDesciption(String strategy) {
+		URL url = TradingStrategyFactory.class.getResource("/definitions/" + strategy + ".txt");
+		String result = "";
+		try {
+			result = FileUtils.read(url, Charset.defaultCharset());
+		} catch (Exception ex) {
+			 LOG.warn("Could not find description for "+strategy+": "+ex);
+		}
+		return result;
+	}
+
+	/**
+	 * Provides a list of the supported strategy names
+	 * @return
+	 */	
+	public static List<String> list() {
+		try {
+			List<String> result = new ArrayList();
+			for (StrategyEnum cl : StrategyEnum.values()) {
+				result.add(cl.name());
+			}
+			return result;
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
