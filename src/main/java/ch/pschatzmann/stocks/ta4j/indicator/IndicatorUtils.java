172c666bc904f76092e82dabab11d9060a55177a
WongLiAtom <88535007+WongLiAtom@users.noreply.github.com>
2023-02-27 18:18:03 +0000

    clean up;

--- src/main/java/ch/pschatzmann/stocks/ta4j/indicator/IndicatorUtils.java
+++ src/main/java/ch/pschatzmann/stocks/ta4j/indicator/IndicatorUtils.java
@@ -2,4 +2,15 @@ package ch.pschatzmann.stocks.ta4j.indicator;
 
 import org.ta4j.core.BaseBarSeries;
 import org.ta4j.core.Indicator;
-import org.ta4j.core.BarSer
+import org.ta4j.core.BarSeries;
+
+public class IndicatorUtils {
+
+	public static BarSeries getTimeSeries(BarSeries ts, int start, int end) {
+		return new BaseBarSeries(ts.getName(), ts.getBarData().subList(start, end));
+	}
+	
+	public static String getName(Indicator indicator) {
+		return (indicator instanceof Name) ? ((Name)indicator).getName() : indicator.getClass().getSimpleName();
+	}
+}
