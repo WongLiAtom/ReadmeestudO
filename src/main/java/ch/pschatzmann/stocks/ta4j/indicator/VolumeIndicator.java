8bcda83a65e988b76d2628f2fd3c872c9813ad87
WongLiAtom <88535007+WongLiAtom@users.noreply.github.com>
2023-03-05 14:10:52 +0000

    AUTO Committing binary APK

--- src/main/java/ch/pschatzmann/stocks/ta4j/indicator/VolumeIndicator.java
+++ src/main/java/ch/pschatzmann/stocks/ta4j/indicator/VolumeIndicator.java
@@ -2,3 +2,25 @@ package ch.pschatzmann.stocks.ta4j.indicator;
 
 import org.ta4j.core.BarSeries;
 import org.ta4j.core.indicators.CachedIndicator;
+import org.ta4j.core.num.Num;
+
+/**
+ * Returns the volume directly from the tick
+ * 
+ * @author pschatzmann
+ *
+ */
+public class VolumeIndicator extends CachedIndicator<Num> implements IIndicator<Num> {
+	private static final long serialVersionUID = 1L;
+	private BarSeries series;
+
+	public VolumeIndicator(BarSeries series) {
+		super(series);
+		this.series = series;
+	}
+
+	@Override
+	protected Num calculate(int index) {
+		return series.getBar(index).getVolume();
+	}
+}
